7911ea9427da508de66d5db8a42563786a5a53d5
Hooman Pegahmehr <hpegahmehr@gmail.com>
2023-09-12 16:57:43 -0400

    added more data types

---
+++ lists.py
@@ -0,0 +1,18 @@
+# helful REPL methods, help, type, and dir
+# list is mutable
+list()
+[]
+type([])
+names = ["Nina", "Max", "Jane"]
+len(names)
+names[0]
+names[0] = "Mark"
+lottery_numbers = [1, 4, 1993, 7]
+sorted(lottery_numbers)
+sorted(lottery_numbers, reverse=True)
+lottery_numbers.sort()
+dir(list)
+lottery_numbers.append(9999)
+lottery_numbers.insert(0, -1)
+lottery_numbers.pop()
+32 in lottery_numbers
