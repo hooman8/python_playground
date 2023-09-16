0b26c8194d707b55f741af2a2d58b0ffe069e11e
Hooman Pegahmehr <hpegahmehr@gmail.com>
2023-09-16 15:47:27 -0400

    type conversion

---
+++ type_conversion.py
@@ -0,0 +1,16 @@
+my_data = "this,is,a,comma,seperated,string"
+my_list = my_data.split(",")
+student = "Mary,8,Math"
+name, age, subject = student.split(",")
+
+" ".join(my_list)
+
+
+greeting = "hello"
+list(greeting)
+"".join(list(greeting))
+
+names = ["nina", "jack", "nina"]
+set(names)
+# sorted give back a list
+sorted(set(names))
