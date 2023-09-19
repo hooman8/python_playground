59b664e09128f7dc9d395d3c7aeadb5d7afd608d
Hooman Pegahmehr <hpegahmehr@gmail.com>
2023-09-19 11:12:41 -0400

    initializer method

--- oop_classes.py
+++ oop_classes.py
@@ -5,16 +5,19 @@ type(int(5))
 class Car:
     runs = True
 
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         print(f"Does run? {self.runs}")
 
-    def start(self, name):
-        self.name = name
+    def start(self):
         if self.runs:
             print(f"{self.name} car is started")
         else:
             print(f"{self.name} car is broken")
 
+
+my_car = Car("Sabura")
+my_car.start()
 
 # inside of the reple
 # from oop_classes import Car
