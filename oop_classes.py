521c22a8ebdda6674f862ed39acdcff529479cae
Hooman Pegahmehr <hpegahmehr@gmail.com>
2023-09-19 14:44:22 -0400

    is instance and subclass

--- oop_classes.py
+++ oop_classes.py
@@ -8,7 +8,7 @@ class Car:
 
     def __init__(self, name):
         self.name = name
-        print(f"Does run? {self.runs}")
+        # print(f"Does run? {self.runs}")
 
     def start(self):
         if self.runs:
@@ -26,8 +26,13 @@ my_car.get_number_of_wheels()
 # my_car.start()
 # inside of the reple
 # from oop_classes import Car
-
+print(isinstance(my_car, Car))
 # import oop_classes
 # import importlib
 # importlib.reload(oop_classes)
 # type(oop_classes.Car)
+
+
+isinstance(True, int)  # booleans are subclass of int
+# True + True <-- this gives you 2
+issubclass(int, object)
