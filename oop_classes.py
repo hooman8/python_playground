5e090606408dd22142e302f82c8dc7b051fc8f56
Hooman Pegahmehr <hpegahmehr@gmail.com>
2023-09-20 16:42:16 -0400

    str and repr

--- oop_classes.py
+++ oop_classes.py
@@ -10,6 +10,12 @@ class Car:
         self.name = name
         # print(f"Does run? {self.runs}")
 
+    def __str__(self):
+        return f"My car the {self.name} currently {self.runs}"
+
+    def __repr__(self):
+        return f"Car({self.name})"
+
     def start(self):
         if self.runs:
             print(f"{self.name} car is started")
@@ -22,17 +28,19 @@ class Car:
 
 
 my_car = Car("Sabura")
-my_car.get_number_of_wheels()
+# print(my_car)
+str(my_car)
+# my_car.get_number_of_wheels()
 # my_car.start()
 # inside of the reple
 # from oop_classes import Car
-print(isinstance(my_car, Car))
+# print(isinstance(my_car, Car))
 # import oop_classes
 # import importlib
 # importlib.reload(oop_classes)
 # type(oop_classes.Car)
 
 
-isinstance(True, int)  # booleans are subclass of int
+# isinstance(True, int)  # booleans are subclass of int
 # True + True <-- this gives you 2
-issubclass(int, object)
+# issubclass(int, object)
---
+++ str_repr.py
@@ -0,0 +1,8 @@
+# for repr, it's how would I tell Python to ccreate a new instance of this class
+# str value can be thought of as the human readable string value
+
+import datetime
+
+now = datetime.datetime.now()
+print(str(now))
+print(repr(now))
